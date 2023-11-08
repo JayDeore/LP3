38b4ef69fa9fc6090bc338145c6f04e2cd1b9c02
Jayesh Deore <93010507+JayDeore@users.noreply.github.com>
2023-11-08 08:48:39 +0530

    Update studentDataStructure.sol

--- BCT/studentDataStructure.sol
+++ BCT/studentDataStructure.sol
@@ -1,47 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract StudentData {
     struct Student {
-        uint256 studentID;
         string name;
+        uint256 rollNumber;
         uint256 age;
     }
-
     Student[] public students;
+    constructor() {}
 
-    // Add a new student record
-    function addStudent(uint256 _studentID, string memory _name, uint256 _age) public {
-        students.push(Student(_studentID, _name, _age));
+    function addStudent(string memory _name, uint256 _rollNumber, uint256 _age) public {
+        Student memory newStudent = Student(_name, _rollNumber, _age);
+        students.push(newStudent);
     }
 
-    // Get the number of students in the array
     function getStudentCount() public view returns (uint256) {
         return students.length;
     }
 
-    // Get student details by index
-    function getStudent(uint256 index) public view returns (uint256, string memory, uint256) {
-        require(index < students.length, "Student index out of bounds");
+    function getStudent(uint256 index) public view returns (string memory, uint256, uint256) {
+        require(index < students.length);
         Student memory student = students[index];
-        return (student.studentID, student.name, student.age);
-    }
-
-    // Fallback function to accept Ether transactions
-    receive() external payable {
-        // Do something with the received Ether, for educational purposes.
-    }
-
-    // Withdraw Ether from the contract (only owner)
-    function withdraw() public {
-        require(msg.sender == owner, "Only the owner can withdraw");
-        payable(msg.sender).transfer(address(this).balance);
-    }
-
-    address public owner;
-
-    constructor() {
-        owner = msg.sender;
+        return (student.name, student.rollNumber, student.age);
     }
 }
-
